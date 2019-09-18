bed733ee448921568b9ff362c1c0d1151d95df81
Varsha.C.Bendre <varsha6319@gmail.com>
2019-09-18 12:22:56 +0530

    Add files via upload

---
+++ MQ135.ino
@@ -0,0 +1,26 @@
+const int gasSensor =0;
+void setup()
+{
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+
+}
+
+void loop()
+{
+  float voltage;
+  voltage = getVoltage(gasSensor);
+
+
+  Serial.println(voltage);
+
+  delay(1000);
+  
+  // put your main code here, to run repeatedly:
+
+}
+float getVoltage(int pin)
+{
+  return(analogRead(pin)*0.004882814);
+}
+
